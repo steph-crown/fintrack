200d9bcaca74239f985f7f8e20b9ee60ac912b55
Steph Crown <emmanuelstephen024@gmail.com>
2025-12-31 01:04:24 +0100

    Implement delete command schema

--- src/commands.rs
+++ src/commands.rs
@@ -4,18 +4,20 @@ use clap::{ArgMatches, Command};
 pub type Exec = fn(&mut GlobalContext, &ArgMatches) -> CliResult;
 
 pub fn cli() -> Vec<Command> {
-  vec![init::cli(), add::cli(), update::cli()]
+  vec![add::cli(), delete::cli(), init::cli(), update::cli()]
 }
 
 pub fn build_exec(cmd: &str) -> Option<Exec> {
   match cmd {
-    "init" => Some(init::exec),
     "add" => Some(add::exec),
+    "delete" => Some(delete::exec),
+    "init" => Some(init::exec),
     "update" => Some(update::exec),
     _ => None,
   }
 }
 
 pub mod add;
+pub mod delete;
 pub mod init;
 pub mod update;
---
+++ src/commands/delete.rs
@@ -0,0 +1,36 @@
+use clap::{Arg, ArgAction, ArgMatches, Command};
+
+use crate::{Category, CliResponse, CliResult, GlobalContext};
+
+pub fn cli() -> Command {
+  Command::new("delete")
+    .about("Modify an existing record")
+    .arg(
+      Arg::new("ids")
+        .help("Comma separated list of record ids")
+        .short('i')
+        .long("ids")
+        .value_parser(clap::value_parser!(usize))
+        .action(ArgAction::Append)
+        .value_delimiter(','),
+    )
+    .arg(
+      Arg::new("by-cat")
+        .help("Specify a category. Deletes record for the category")
+        .short('c')
+        .long("category")
+        .value_parser(clap::value_parser!(Category)),
+    )
+    .arg(
+      Arg::new("by-subcat")
+        .help("Specify a subcategory. Deletes record for the subcategory")
+        .short('s')
+        .long("subcategory")
+        .value_parser(clap::value_parser!(String)),
+    )
+}
+
+pub fn exec(gctx: &mut GlobalContext, args: &ArgMatches) -> CliResult {
+  println!("{:#?} fuck {:#?}", gctx, args);
+  Ok(CliResponse { success: true })
+}
